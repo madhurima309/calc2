f5094163345d5c34196b7a60e43d37e11021879a
Madhurimasingh1 <67088283+Madhurimasingh1@users.noreply.github.com>
2021-11-09 17:51:05 -0500

    refactor to use calculation class

---
+++ calc/history/calculations.py
@@ -0,0 +1,27 @@
+"""Calculation history Class"""
+
+
+# pylint: disable=too-few-public-methods,missing-class-docstring,missing-function-docstring
+class Calculations:
+    history = []
+    @staticmethod
+    def clear_history():
+        Calculations.history.clear()
+        return True
+    @staticmethod
+    def count_history():
+        return len(Calculations.history)
+    @staticmethod
+    def get_last_calculation():
+        return Calculations.history[-1]
+    @staticmethod
+    def get_first_calculation():
+        return Calculations.history[-1]
+    @staticmethod
+    def get_calculation(num):
+        """ get a specific calculation from history"""
+        return Calculations.history[num]
+    @staticmethod
+    def add_calculation(calculation):
+        """ get a specific calculation from history"""
+        return Calculations.history.append(calculation)
